9486f2ed27bfab41e30cc5a8709cc5302222c1ef
Mattbil2003 <90293237+Mattbil2003@users.noreply.github.com>
2024-02-25 21:15:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 app.get('/roll', (req, res) => {
